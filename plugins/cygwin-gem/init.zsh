53a805254131417f415fa1b8bf37e8f7c7930dfe
Larry Gordon <larry.gordon@xero.com>
2014-09-29 15:01:30 -0700

    Fixes cygwin-gem plugin

--- plugins/cygwin-gem/init.zsh
+++ plugins/cygwin-gem/init.zsh
@@ -24,10 +24,18 @@ if [[ -n "$(which ruby 2>/dev/null)" ]]; then
 fi
 
 
-# Lists all gems and associated executables installed with gem
+# Lists all gems and associated executables installed with gem returned as
+# a string one:two:three
 # --------------------------------------------------------------------------
 _gem-list() {
   echo "${$(ls "$ALF_RUBY_BIN" | grep ".bat$" | tr "\n" ":")%:}"
+}
+
+# Lists all gems and associated executables installed with gem returned as
+# an array (one two three)
+# --------------------------------------------------------------------------
+_gem-list-array() {
+  echo "${$(ls "$ALF_RUBY_BIN" | grep ".bat$" | tr "\n" " ")%:}"
 }
 
 # Manage aliases for installed gems
@@ -39,48 +47,47 @@ _gem-alias() {
     uninstall_alias=1
   fi
   shift
-  local args_list=()
+  args_list=()
   if [[ ${#@} > 0 ]]; then
     args_list=( $(echo $@ | tr ":" " ") )
   fi
-  local curr_gems=()
-  for curr_gem in $(_gem-list | tr ":" " "); do
-    curr_gems+=( "$curr_gem" )
-  done
-  unset curr_gem
-  local diff_gems=()
+  curr_gems=( $(_gem-list-array) )
   # Add gem.bat if the array is empty so that it can be filtered out later.
   # We do this because we have a function called "gem" and we don't need
   # an alias to replace this function.
-  [[ ${#args_list} == 0 ]] && args_list=( gem.bat )
-  local gems_list_sm=()
-  local gems_list_lg=()
-  # Had to do a weird assignment hack to get this working right
-  for gem_sm in ${gems_list_sm[@]}; do
-    gems_list_sm+=( "$gem_sm" )
-  done
-  for gem_lg in ${gems_list_lg[@]}; do
-    gems_list_lg+=( "$gem_lg" )
-  done
-  # Find the largest array
-  if [[ ${#gems_list_sm} -gt ${#gems_list_lg} ]]; then
+  [[ ${#args_list[@]} == 0 ]] && args_list=( gem.bat )
+  gems_list_sm=()
+  gems_list_lg=()
+  if [[ ${#args_list[@]} -gt ${#curr_gems[@]} ]]; then
     gems_list_sm=("${curr_gems[@]}")
     gems_list_lg=("${args_list[@]}")
+  else
+    gems_list_sm=("${args_list[@]}")
+    gems_list_lg=("${curr_gems[@]}")
   fi
   # Filter out the unique elements
+  diff_gems=()
   for gem_file_bat in "${gems_list_lg[@]}"; do
     if [[ ! -n $(grep "$gem_file_bat" <<< "${gems_list_sm[@]}") ]]; then
-      diff_gems=("${diff_gems[@]}" $gem_file_bat)
+      diff_gems+=( "$gem_file_bat" )
     fi
   done
-  # Add or remove alias
-  for gem_file_bat in $diff_gems; do
-    if [[ -n $uninstall_alias ]]; then
-      unalias "${gem_file_bat%.bat}"
-    else
+
+  if [[ -n $uninstall_alias ]]; then
+    # Remove alias
+    for gem_file_bat in $diff_gems; do
+      if [[ -n $(alias | grep "${gem_file_bat%.bat}") ]]; then
+        unalias "${gem_file_bat%.bat}"
+      fi
+    done
+  else
+    # Add alias
+    for gem_file_bat in $diff_gems; do
       alias "${gem_file_bat%.bat}"="$ALF_RUBY_BIN/$gem_file_bat"
-    fi
-  done
+    done
+  fi
+  unset uninstall_alias
+  unset gem_file_bat
   unset gems_list_sm
   unset gems_list_lg
   unset args_list
@@ -98,6 +105,8 @@ gem() {
   local gem_file_bats=`_gem-list`
   "$ALF_RUBY_BIN/gem.bat" $@
   if [[ -n $(echo $1 | grep "install") ]]; then
-    { _gem-alias $1 "$gem_file_bats" } &!
+    _gem-alias $1 "$gem_file_bats"
   fi
 }
+
+_gem-alias "install"
